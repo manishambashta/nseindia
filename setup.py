c5d141595970c46a764baa58a71641f99872f6d8
Manish <manish.hitk@gmail.com>
2016-12-08 19:52:36 +0530

    added requirements and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='NSEIndia',
+      version='1.0',
+      description='Top NSE Gainers at current moment',
+      author='Manish Kumar',
+      author_email='manish.hitk@gmail.com',
+     )
